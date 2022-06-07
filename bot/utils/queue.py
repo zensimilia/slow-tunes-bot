c0032bfc13896e0b7fc3c3be7a965dbc5901c438
Di M Dub <zensimilia@gmail.com>
2022-06-07 14:13:34 +0300

    Run the queue on start

--- bot/utils/queue.py
+++ bot/utils/queue.py
@@ -1,33 +1,48 @@
 import asyncio
-from typing import Union
 
 from bot.utils.logger import get_logger
+from bot.utils.singleton import singleton
 
 log = get_logger()
 
 
-class BotQueue:
-    """A class that implements queue for telegram bot."""
+@singleton
+class Queue:
+    """A class that implements async queue."""
 
     def __init__(
         self,
         maxsize: int = 0,
-        name: Union[str, None] = None,
     ) -> None:
         self._queue = asyncio.Queue(maxsize=maxsize)
-        self.name = name
 
     async def start(self):
+        """Starts loop worker for the queue."""
+
+        log.debug("Start the worker for main queue")
         while True:
             try:
                 task = await self._queue.get()
-                self._queue.task_done()
-                yield task
+                log.debug("Run Task from the queue %s", task)
+                await asyncio.create_task(task)
             except (asyncio.CancelledError, ValueError) as error:
                 log.warning(error)
 
     async def enqueue(self, func, *args, **kwargs) -> int:
-        task = asyncio.create_task(func(*args, **kwargs))
-        await self._queue.put(task)
+        """Add a task into the queue."""
+
+        await self._queue.put(func(*args, **kwargs))
+        log.debug("New Task added to the queue")
+        return self.size
+
+    @property
+    def size(self):
+        """Return the number of items in the queue."""
 
         return self._queue.qsize()
+
+    @property
+    def is_empty(self):
+        """Return `True` if the queue is empty, `False` otherwise."""
+
+        return self._queue.empty()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
@@ -5,12 +6,12 @@ from bot.config import AppConfig
 from bot.db import init_sqlite
 from bot.handlers import register_handlers
 from bot.utils.logger import get_logger
-from bot.utils.queue import BotQueue
+from bot.utils.queue import Queue
 
 log = get_logger(__name__)
 config = AppConfig()
 storage = MemoryStorage()
-queue = BotQueue()
+queue = Queue()
 
 
 async def on_startup(dp: Dispatcher):
@@ -28,23 +29,14 @@ async def on_startup(dp: Dispatcher):
     ]
     await dp.bot.set_my_commands(commands)
 
-    test1 = await queue.enqueue(test, "lol")
-    test2 = await queue.enqueue(test, "rolf")
-    print(test1)
-    print(test2)
+    # Starts loop worker for the queue
+    asyncio.create_task(queue.start())
 
 
 async def on_shutdown(_dp: Dispatcher):
     """Execute function before Bot shut down polling."""
 
     log.info("Execute shutdown Bot functions...")
-
-
-async def test(text):
-    for x in range(30000000):
-        y = x ^ x
-    print(f"test: {text}")
-    return f"return: {text}"
 
 
 def main():
@@ -59,7 +51,6 @@ def main():
         on_startup=on_startup,
         on_shutdown=on_shutdown,
     )
-    queue.start()
 
 
 if __name__ == '__main__':
