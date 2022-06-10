e12942119bd3598331991edcf86513b019124b89
Di M Dub <zensimilia@gmail.com>
2022-06-10 14:04:12 +0300

    Answer to reply for errors

--- bot/errors.py
+++ bot/errors.py
@@ -9,7 +9,7 @@ async def global_error_handler(update: types.Update, error: Exception):
     """Global errors handler."""
 
     log.error(error)
-    await update.message.answer(
+    await update.message.reply(
         "😱 Something wrong happened! Please try again or come back later..."
     )
     return True
@@ -23,14 +23,14 @@ async def file_is_too_big(update: types.Update, _error: Exception):
         update.message.audio.file_id,
         update.message.audio.file_size,
     )
-    await update.message.answer("💾 File is too big. Max file size is 20 MB.")
+    await update.message.reply("💾 File is too big. Max file size is 20 MB.")
     return True
 
 
 async def database_error(update: types.Update, _error: Exception):
     """Error handler for database Error exception."""
 
-    await update.message.answer(
+    await update.message.reply(
         "🚧 I have some issues with the database. Please come back later..."
     )
     return True
