f9cbd6077b8fa46bd3fcfa2d0ffa13ef79569008
Di M Dub <zensimilia@gmail.com>
2022-06-10 14:03:58 +0300

    Own queue counter

--- bot/utils/queue.py
+++ bot/utils/queue.py
@@ -8,39 +8,43 @@ log = get_logger()
 
 @singleton
 class Queue:
-    """A class that implements async queue."""
+    """A class that implements async queue for tasks."""
 
     def __init__(
         self,
         maxsize: int = 0,
     ) -> None:
         self._queue = asyncio.Queue(maxsize=maxsize)
+        self._queue_size = 0
 
     async def start(self):
         """Starts loop worker for the queue."""
 
-        log.debug("Start the worker for main queue")
+        log.debug("Start worker for the main queue")
 
         while True:
             try:
-                task = await self._queue.get()
-                log.debug("Run Task from the queue %s", task)
-                await asyncio.create_task(task)
-            except (asyncio.CancelledError, ValueError) as error:
-                log.warning(error)
+                coro = await self._queue.get()
+                log.debug("Run Task from the queue %s", coro)
+                await asyncio.create_task(coro)
+            except (asyncio.CancelledError, ValueError):
+                pass
+            finally:
+                self._queue_size -= 1
 
     async def enqueue(self, func, *args, **kwargs) -> int:
         """Add a task into the queue."""
 
         await self._queue.put(func(*args, **kwargs))
-        log.debug("New Task added to the queue")
-        return self.size
+        self._queue_size += 1
+        log.debug("Task added to the queue %s", func)
+        return self._queue_size
 
     @property
     def size(self):
         """Return the number of items in the queue."""
 
-        return self._queue.qsize()
+        return self._queue_size
 
     @property
     def is_empty(self):
