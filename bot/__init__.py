fb8e833b4b92f87332762a87ce4acccbfa5ea27a
Di M Dub <zensimilia@gmail.com>
2022-06-19 17:53:58 +0300

    Add version and description for Bot module

---
+++ bot/__init__.py
@@ -0,0 +1,9 @@
+"""The Slow Tunes Bot.
+
+This telegram Bot slowing down audio tracks at 45/33 rpm vinyl ratio
+for aesthetic pleasure. Also Bot allows to share slowed audio to
+other users.
+"""
+
+__version__ = "2022.06.19"
+__author__ = "Dubovik Dmitry"
