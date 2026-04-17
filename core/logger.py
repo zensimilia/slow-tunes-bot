c37bf1bdd1dfe12f4757332cf4927ce04b1e9230
Di M Dub <zensimilia@gmail.com>
2026-04-17 13:13:32 +0300

    Intercepts logging messages and redirects them to loguru

--- core/logger.py
+++ core/logger.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 from loguru import logger
@@ -7,10 +8,47 @@ from .config import config
 LOG_LEVEL = "DEBUG" if config.DEBUG else "INFO"
 
 
+class InterceptHandler(logging.Handler):
+    """
+    A logging handler that intercepts standard logging messages and redirects them to loguru.
+    """
+
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame = logging.currentframe()
+        depth = 0
+
+        while frame:
+            filename = frame.f_code.co_filename
+            if filename in (logging.__file__, __file__):
+                frame = frame.f_back
+                depth += 1
+            else:
+                break
+
+        logger.opt(
+            depth=depth,
+            exception=record.exc_info,
+        ).log(
+            level,
+            record.getMessage(),
+        )
+
+
 def setup_logging() -> None:
     """
     Sets up logging configuration with a specific format and log level.
     """
+
+    logging.basicConfig(
+        handlers=[InterceptHandler()],
+        level=0,
+        force=True,
+    )  # logging -> loguru
 
     logger.remove()  # Remove a previously added handlers
     logger.add(
