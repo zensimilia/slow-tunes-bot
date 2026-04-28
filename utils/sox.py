53b0af58d749fde3ef986d6afecc76dae736fe4d
Di M Dub <zensimilia@gmail.com>
2026-04-28 09:58:43 +0300

    Tune sox

--- utils/sox.py
+++ utils/sox.py
@@ -1,7 +1,8 @@
 import asyncio
 import io
 
-SOX_FMT: list[str] = ["aif", "aifc", "aiff", "aiffc", "flac", "mp2", "mp3", "ogg", "opus", "vorbis"]
+SOX_FMT = ["aif", "aifc", "aiff", "aiffc", "flac", "mp2", "mp3", "ogg", "opus", "vorbis"]
+OUTPUT_MP3_QUALITY = "-0.9"
 
 
 class SoxException(Exception):
@@ -9,17 +10,18 @@ class SoxException(Exception):
 
 
 def get_sox_cli_args(in_fmt: str = "mp3") -> list[str]:
-    io = ["-t", in_fmt, "-", "-t", "mp3", "-C", "-0.9", "-"]  # Output to MP3 directly in sox
+    io = ["-t", in_fmt, "-", "-t", "mp3", "-C", OUTPUT_MP3_QUALITY, "-"]  # Output to MP3 directly in sox
+    bass = ["bass", "+3"]  # Gain bass in dB
+    pad = ["pad", "0", "2"]  # Add silence at the end of the track
+    highpass = ["highpass", "50"]  # High-pass filter
+    gain = ["gain", "-1"]  # Reduce the overall gain to prevent clipping
+    speed = ["speed", str(33 / 45)]  # Adjust the speed of the audio
+    norm = ["norm", "-1"]  # Normalize the audio
+
     # Reverberence, HF damping, Room scale, Stereo depth, Pre delay, Wet gain
     reverb = ["reverb", "70", "30", "100", "50"]
-    bass = ["bass", "+3"]  # Gain bass in dB
-    pad = ["pad", "0", "3"]  # Add 3 seconds of silence at the end of the track
-    highpass = ["highpass", "50"]  # High-pass filter with a cutoff frequency of 50 Hz
-    _gain = ["gain", "-3"]  # Reduce the overall gain by 3 dB to prevent clipping
-    speed = ["speed", str(33 / 45)]  # Adjust the speed of the audio
-    norm = ["norm", "-1"]  # Normalize the audio to -1 dB to prevent clipping after speed change
 
-    return ["-V1", *io, *speed, *highpass, *reverb, *bass, *pad, *norm]
+    return ["-V1", *io, *speed, *gain, *highpass, *reverb, *bass, *pad, *norm]
 
 
 async def proceed_audio(input_buffer: io.BytesIO, fmt: str = "mp3") -> bytes:
