9bb49c552d758e24362629c572671e6965e101ed
Di M Dub <zensimilia@gmail.com>
2026-05-02 15:16:58 +0300

    Refactor TaskStream

--- core/stream.py
+++ core/stream.py
@@ -27,6 +27,7 @@ class TaskStream:
         self.__groupname = groupname
         self.__streams = {}
         self.__running = False
+        self.__tasks = set()
 
     async def subscribe(self, name: str, func: TaskFunc, *, streamid: str = "$") -> None:
         with contextlib.suppress(ResponseError):
@@ -44,9 +45,11 @@ class TaskStream:
 
     async def __run(self, func: TaskFunc, payload: dict) -> None:
         sig = inspect.signature(func)
-        data = {k: v for k, v in payload.items() if k in sig.parameters}
+        has_kwargs = any(p.kind == p.VAR_KEYWORD for p in sig.parameters.values())
 
-        if "bot" in sig.parameters:
+        data = payload if has_kwargs else {k: v for k, v in payload.items() if k in sig.parameters}
+
+        if "bot" in sig.parameters or has_kwargs:
             data["bot"] = self.__bot
 
         if inspect.iscoroutinefunction(func):
@@ -71,9 +74,14 @@ class TaskStream:
 
         for stream, message in events:
             for msg_id, payload in message:
+                decoded_payload = {
+                    k.decode() if isinstance(k, bytes) else k: v.decode() if isinstance(v, bytes) else v
+                    for k, v in payload.items()
+                }
                 logger.debug(f"Processing task #{msg_id} from {stream}")
-                task = asyncio.create_task(self.__process_task(stream, msg_id, payload))
-                task.set_name("task")
+                task = asyncio.create_task(self.__process_task(stream, msg_id, decoded_payload))
+                self.__tasks.add(task)
+                task.add_done_callback(self.__tasks.discard)
 
     async def listen(self, consumer_name: str) -> None:
         if not self.__streams:
@@ -100,8 +108,11 @@ class TaskStream:
 
     async def stop(self, *, purge: bool = False) -> None:
         self.__running = False
+        if self.__tasks:
+            await asyncio.wait(self.__tasks, timeout=5)
         if purge:
             [await self.unsubscribe(s) for s in self.__streams]
 
     async def unsubscribe(self, name: str) -> None:
         await self.__r.xgroup_destroy(name, self.__groupname)
+        logger.info(f"Unsubscribe '{name}' from '{self.__groupname}'")
