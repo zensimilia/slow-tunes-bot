38e7b0532ad3c60e4bda849c18cd28e819bc5279
Zensimilia <zensimilia@gmail.com>
2026-05-09 02:09:58 +0300

    FileIsTooBigError

--- bot/core/exceptions.py
+++ bot/core/exceptions.py
@@ -1,4 +1,9 @@
-class FileIsTooBigError(Exception): ...
+class FileIsTooBigError(Exception):
+    """Raised when a file exceeds a certain size limit."""
+
+    def __init__(self, file_size: int) -> None:
+        size_mib = file_size / (1024 * 1024)
+        super().__init__(f"File size ({size_mib:.2f} MiB) is too big")
 
 
 class UnsupportedFormatError(Exception): ...
@@ -10,6 +15,8 @@ class DownloadError(Exception): ...
 class UploadError(Exception): ...
 
 
-class MissingRequiredError(ValueError):
+class MissingRequiredError(ValueError, KeyError):
+    """Raised when object didn't have required key or value."""
+
     def __init__(self) -> None:
         super().__init__("There is no required object, key or value")
